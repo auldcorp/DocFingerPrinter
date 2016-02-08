76f803287f95f51c011cf81c9ba160c4b4f98aa3
grogers1993 <grogers1993@gmail.com>
2016-02-07 23:16:17 -0500

    Set up webpage.

---
+++ DocFingerPrinterBeta/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace DocFingerPrinterBeta.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public int ImageMark { get; set; }
+    }
+
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
