9eff2d31fa1873b1b0ad5c336218b04ddbb434cd
Thomas Tedrow <tedrowtom@gmail.com>
2016-02-22 15:24:48 -0500

    added DAL

--- DocFingerPrinterBeta/Controllers/ImageUploadController.cs
+++ DocFingerPrinterBeta/Controllers/ImageUploadController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DocFingerPrinterBeta.Models;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -10,9 +11,11 @@ namespace DocFingerPrinterBeta.Controllers
 {
     public class ImageUploadController : Controller
     {
+        private MyDbContext db = new MyDbContext();
         // GET: ImageUpload
         public ActionResult Index()
         {
+
             return View();
         }
 
@@ -30,6 +33,7 @@ namespace DocFingerPrinterBeta.Controllers
                 string openstegoPath = "\"C:\\Program Files (x86)\\OpenStego\\lib\\openstego.jar\"";
                 string secretTextPath = Path.Combine(Server.MapPath("~/texts"), "secretText.txt");
 
+
                 var proc1 = new ProcessStartInfo();
                 string embedCommand = "java -jar " +openstegoPath + " embed -a RandomLSB -mf \"" +secretTextPath + "\" -cf \"" +path + "\" -sf \"C:\\Users\\Public\\test.png\"";
                 proc1.UseShellExecute = true;
@@ -41,18 +45,28 @@ namespace DocFingerPrinterBeta.Controllers
                 proc1.Arguments = "/c " + embedCommand;
                 proc1.WindowStyle = ProcessWindowStyle.Hidden;
                 Process.Start(proc1);
+                Image newImage = new Image();
 
                 using (MemoryStream ms = new MemoryStream())
                 {
                     file.InputStream.CopyTo(ms);
                     byte[] array = ms.GetBuffer();
+                    newImage.imageBinary = array;
+                    newImage.filename = image;
+                    try
+                    {
+                        db.Images.Add(newImage);
+                        db.SaveChanges();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Write(e);
+                    }
                 }
 
             }
 
-            //file has been uploaded now do opensteg on image to mark it
-            //then redirect back to where ever
-            return RedirectToAction("Index", "ImageUpload");
+            ;
         }
     }
 }
