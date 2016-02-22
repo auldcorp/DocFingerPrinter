c20e02544656e49afb65ed4ddcde7aac4b2f5c2f
Thomas Tedrow <tedrowtom@gmail.com>
2016-02-22 15:29:51 -0500

    added excluded files

--- DocFingerPrinterBeta/Controllers/ImageUploadController.cs
+++ DocFingerPrinterBeta/Controllers/ImageUploadController.cs
@@ -1,4 +1,5 @@
-﻿using DocFingerPrinterBeta.Models;
+﻿using DocFingerPrinterBeta.DAL;
+using DocFingerPrinterBeta.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
---
+++ DocFingerPrinterBeta/DAL/MyDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DocFingerPrinterBeta.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocFingerPrinterBeta.DAL
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Image> Images { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+
+    }
+}
