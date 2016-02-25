107d9146ebc816145bfe051069db2e9e9d478139
Gordish <nchockeydude27@hotmail.com>
2016-02-24 23:47:15 -0500

    Started CommandPrompt class

--- DocFingerPrinterBeta/Controllers/ImageUploadController.cs
+++ DocFingerPrinterBeta/Controllers/ImageUploadController.cs
@@ -1,5 +1,6 @@
 ﻿using DocFingerPrinterBeta.DAL;
 using DocFingerPrinterBeta.Models;
+using DocFingerPrinterBeta.Static_Classes;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -18,39 +19,30 @@ namespace DocFingerPrinterBeta.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Upload Page";
+            ViewBag.Link = "";
+            ViewBag.Hidden = "display: none";
             return View();
         }
-
 
         public ActionResult FileUpload(HttpPostedFileBase file)
         {
             if (file != null)
             {
                 string image = Path.GetFileName(file.FileName);
-                string path = Path.Combine(
-                                       Server.MapPath("~/images/profile"), image);
+                string path = Path.Combine(Server.MapPath("~/images/profile"), image);
 
                 file.SaveAs(path);
 
                 string openstegoPath = "\"C:\\Program Files (x86)\\OpenStego\\lib\\openstego.jar\"";
                 string secretTextPath = Path.Combine(Server.MapPath("~/texts"), "secretText.txt");
-
-
-                var proc1 = new ProcessStartInfo();
                 string embedCommand = "java -jar " +openstegoPath + " embed -a RandomLSB -mf \"" +secretTextPath + "\" -cf \"" +path + "\" -sf \"C:\\Users\\Public\\test.png\"";
-                proc1.UseShellExecute = true;
+                string workingDirectory = @"C:\Users\Public";
 
-                proc1.WorkingDirectory = @"C:\Users\Public";
-
-                proc1.FileName = @"C:\Windows\System32\cmd.exe";
-
-                proc1.Arguments = "/c " + embedCommand;
-                proc1.WindowStyle = ProcessWindowStyle.Hidden;
-                Process.Start(proc1);
-                Image newImage = new Image();
+                var result = CommandPrompt.ExecuteCommand(embedCommand, workingDirectory);
 
                 using (MemoryStream ms = new MemoryStream())
                 {
+                    Image newImage = new Image();
                     file.InputStream.CopyTo(ms);
                     byte[] array = ms.GetBuffer();
                     newImage.imageBinary = array;
@@ -66,13 +58,11 @@ namespace DocFingerPrinterBeta.Controllers
                     }
                 }
 
-                //Show user encoded file
-                return base.File("C:\\Users\\Public\\test.png", "image/png");
             }
+            ViewBag.Link = "C:\\Users\\Public\\test.png";
+            ViewBag.Hidden = "";
 
-            //file has been uploaded now do opensteg on image to mark it
-            //then redirect back to where ever
-            return RedirectToAction("Index", "ImageUpload");
+            return View("Index");
         }
     }
 }
---
+++ DocFingerPrinterBeta/Static_Classes/CommandPrompt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace DocFingerPrinterBeta.Static_Classes
+{
+    public enum CommandPromptStatus { Success, Error}
+    public static class CommandPrompt
+    {
+        public static CommandPromptStatus ExecuteCommand(string command, string workingDirectory)
+        {
+            var procedure = new ProcessStartInfo();
+
+            procedure.FileName = @"C:\Windows\System32\cmd.exe";
+            procedure.WorkingDirectory = workingDirectory;
+            procedure.UseShellExecute = true;
+            procedure.Arguments = "/c " + command;
+            procedure.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process.Start(procedure);
+            return CommandPromptStatus.Success;
+        }
+    }
+}
