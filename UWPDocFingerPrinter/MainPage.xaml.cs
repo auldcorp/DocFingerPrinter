b4681ad6f33cd091824cceda71c1825a29f2b297
Gordish <nchockeydude27@hotmail.com>
2016-03-09 16:35:00 -0500

    Added functionality to navigation buttons in UWP app

--- UWPDocFingerPrinter/MainPage.xaml.cs
+++ UWPDocFingerPrinter/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,6 +24,9 @@ namespace UWPDocFingerPrinter
     public sealed partial class MainPage : Page
     {
 
+        private SolidColorBrush DarkGrayBrush = new SolidColorBrush(Colors.DarkGray);
+        private SolidColorBrush GrayBrush = new SolidColorBrush(Colors.Gray);
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -31,6 +35,54 @@ namespace UWPDocFingerPrinter
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
             MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void embedSignature_Click(object sender, RoutedEventArgs e)
+        {
+            embedPanel.Background = DarkGrayBrush;
+            settingPanel.Background = GrayBrush;
+            filesPanel.Background = GrayBrush;
+            searchPanel.Background = GrayBrush;
+            SearchPresenter.IsOpen = false;
+            SettingsPresenter.IsOpen = false;
+            MarkedFilesPresenter.IsOpen = false;
+            embedContent.Opacity = 1;
+        }
+
+        private void searchImage_Click(object sender, RoutedEventArgs e)
+        {
+            embedPanel.Background = GrayBrush;
+            settingPanel.Background = GrayBrush;
+            filesPanel.Background = GrayBrush;
+            searchPanel.Background = DarkGrayBrush;
+            SearchPresenter.IsOpen = true;
+            SettingsPresenter.IsOpen = false;
+            MarkedFilesPresenter.IsOpen = false;
+            embedContent.Opacity = 0;
+        }
+
+        private void markedFiles_Click(object sender, RoutedEventArgs e)
+        {
+            embedPanel.Background = GrayBrush;
+            settingPanel.Background = GrayBrush;
+            filesPanel.Background = DarkGrayBrush;
+            searchPanel.Background = GrayBrush;
+            SearchPresenter.IsOpen = false;
+            SettingsPresenter.IsOpen = false;
+            MarkedFilesPresenter.IsOpen = true;
+            embedContent.Opacity = 0;
+        }
+
+        private void settings_Click(object sender, RoutedEventArgs e)
+        {
+            embedPanel.Background = GrayBrush;
+            settingPanel.Background = DarkGrayBrush;
+            filesPanel.Background = GrayBrush;
+            searchPanel.Background = GrayBrush;
+            SearchPresenter.IsOpen = false;
+            SettingsPresenter.IsOpen = true;
+            MarkedFilesPresenter.IsOpen = false;
+            embedContent.Opacity = 0;
         }
     }
 }
