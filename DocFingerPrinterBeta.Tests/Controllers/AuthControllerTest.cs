88d7ed9cf8a8642418274f4cf32ef3f1364a2f07
Thomas Tedrow <tedrowtom@gmail.com>
2016-03-09 11:34:49 -0500

    added some more test cases for authcontrollertest class

--- DocFingerPrinterBeta.Tests/Controllers/AuthControllerTest.cs
+++ DocFingerPrinterBeta.Tests/Controllers/AuthControllerTest.cs
@@ -20,7 +20,6 @@ namespace DocFingerPrinterBeta.Tests.Controllers
         [TestMethod]
         public void RegisterTest()
         {
-
             var mockStore = new Mock<IUserStore<User, int>>();
             var userManager = new UserManager<User,int>(mockStore.Object);
             AuthController testController = new AuthController(userManager);
@@ -29,6 +28,27 @@ namespace DocFingerPrinterBeta.Tests.Controllers
             testModel.Password = "password";
             var result = testController.Register(testModel) as Task<ActionResult>;
             Assert.IsNotNull(result);
+        }
+
+        public void RegisterTestWithoutPassword()
+        {
+            var mockStore = new Mock<IUserStore<User, int>>();
+            var userManager = new UserManager<User, int>(mockStore.Object);
+            AuthController testController = new AuthController(userManager);
+            RegisterModel testModel = new RegisterModel();
+            testModel.Email = "blah@gmail.com";
+            var result = testController.Register(testModel) as Task<ActionResult>;
+            Assert.IsTrue(result.IsFaulted);
+        }
+
+        public void RegisterTestEmpty()
+        {
+            var mockStore = new Mock<IUserStore<User, int>>();
+            var userManager = new UserManager<User, int>(mockStore.Object);
+            AuthController testController = new AuthController(userManager);
+            RegisterModel testModel = new RegisterModel();
+            var result = testController.Register(testModel) as Task<ActionResult>;
+            Assert.IsTrue(result.IsFaulted);
         }
 
         [TestMethod]
@@ -40,6 +60,29 @@ namespace DocFingerPrinterBeta.Tests.Controllers
             LogInModel testModel = new LogInModel();
             testModel.Email = "blah@gmail.com";
             testModel.Password = "password";
+            var result = testController.LogIn(testModel) as Task<ActionResult>;
+            Assert.IsTrue(result.IsCompleted);
+        }
+
+        [TestMethod]
+        public void SignInTestEmpty()
+        {
+            var mockStore = new Mock<IUserStore<User, int>>();
+            var userManager = new UserManager<User, int>(mockStore.Object);
+            AuthController testController = new AuthController(userManager);
+            LogInModel testModel = new LogInModel();
+            var result = testController.LogIn(testModel) as Task<ActionResult>;
+            Assert.IsTrue(result.IsFaulted);
+        }
+
+        [TestMethod]
+        public void SignInTestWithoutPassword()
+        {
+            var mockStore = new Mock<IUserStore<User, int>>();
+            var userManager = new UserManager<User, int>(mockStore.Object);
+            AuthController testController = new AuthController(userManager);
+            LogInModel testModel = new LogInModel();
+            testModel.Email = "blah@gmail.com";
             var result = testController.LogIn(testModel) as Task<ActionResult>;
             Assert.IsTrue(result.IsCompleted);
         }
--- DocFingerPrinterBeta/Controllers/ImageUploadController.cs
+++ DocFingerPrinterBeta/Controllers/ImageUploadController.cs
@@ -30,45 +30,8 @@ namespace DocFingerPrinterBeta.Controllers
         {
             if (file != null)
             {
-                string imageName = Path.GetFileName(file.FileName);
-                string imagePath = Path.Combine(Server.MapPath("~/images/profile"), imageName);
 
-                file.SaveAs(imagePath);
-
-                string openstegoPath = "\"C:\\Program Files (x86)\\OpenStego\\lib\\openstego.jar\"";
-                string secretTextPath = Path.Combine(Server.MapPath("~/texts"), "secretText.txt");
-
-
-                var proc1 = new ProcessStartInfo();
-                string embedCommand = "java -jar " +openstegoPath + " embed -a RandomLSB -mf \"" +secretTextPath + "\" -cf \"" +path + "\" -sf \"C:\\Users\\Public\\test.png\"";
-                proc1.UseShellExecute = true;
-
-                proc1.WorkingDirectory = @"C:\Users\Public";
-
-                proc1.FileName = @"C:\Windows\System32\cmd.exe";
-
-                proc1.Arguments = "/c " + embedCommand;
-                proc1.WindowStyle = ProcessWindowStyle.Hidden;
-                Process.Start(proc1);
-                Image newImage = new Image();
-
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    file.InputStream.CopyTo(ms);
-                    byte[] array = ms.GetBuffer();
-                    newImage.imageBinary = array;
-                    newImage.filename = image;
-                    try
-                    {
-                        db.Images.Add(newImage);
-                        db.SaveChanges();
-                    }
-                    catch (Exception e)
-                    {
-                    //do error handling here
-                    }
-                }
-
+            }
 
             ViewBag.Link = "C:\\Users\\Public\\test.png";
             ViewBag.Hidden = "";
