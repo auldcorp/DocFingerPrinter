e472855eb0c4bc290da310af352f80ccb22967e6
grogers1993 <grogers1993@gmail.com>
2016-03-10 18:27:37 -0500

    Added upload image layout

--- AndroidApplication/DocFingerPrinterBeta/app/src/main/java/auldcorporation/docfingerprinterbeta/Login.java
+++ AndroidApplication/DocFingerPrinterBeta/app/src/main/java/auldcorporation/docfingerprinterbeta/Login.java
@@ -17,15 +17,7 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
     }
 
     @Override
