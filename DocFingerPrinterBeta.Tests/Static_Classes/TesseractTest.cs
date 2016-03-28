2fc4fd2fc04ec98d2530b69d3802b1c436a5c4c5
grogers1993 <grogers1993@gmail.com>
2016-03-27 20:09:55 -0400

    "Added tesseract class and tests"

---
+++ DocFingerPrinterBeta.Tests/Static_Classes/TesseractTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocFingerPrinterBeta.Static_Classes;
+using System.IO;
+
+namespace DocFingerPrinterBeta.Tests.Static_Classes
+{
+    [TestClass]
+    public class TesseractTest
+    {
+        [TestMethod]
+        public void getTextTest()
+        {
+            string imagePath = Directory.GetCurrentDirectory() + "\\Images\\Profile\\landscapeMarked.png";
+            string str = TesseractDetection.getText(imagePath);
+            str = TesseractDetection.removeWhiteSpaces(str);
+            str = TesseractDetection.removeNewLineCharacters(str);
+            Assert.IsNotNull(str);
+            Assert.AreEqual("\\|/|//#|/|", str);
+        }
+
+        [TestMethod]
+        public void getUserIDTest()
+        {
+            string imagePath = Directory.GetCurrentDirectory() + "\\Images\\Profile\\landscapeMarked.png";
+            string str = TesseractDetection.getText(imagePath);
+            str = TesseractDetection.getUserIDString(str);
+            Assert.IsNotNull(str);
+            Assert.AreEqual("|/|//", str);
+        }
+
+        [TestMethod]
+        public void getImageIDTest()
+        {
+            string imagePath = Directory.GetCurrentDirectory() + "\\Images\\Profile\\landscapeMarked.png";
+            string str = TesseractDetection.getText(imagePath);
+            str = TesseractDetection.getImageIDString(str);
+            Assert.IsNotNull(str);
+            Assert.AreEqual("|/|", str);
+        }
+    }
+}
---
+++ DocFingerPrinterBeta/Static_Classes/TesseractDetection.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using Tesseract;
+
+namespace DocFingerPrinterBeta.Static_Classes
+{
+    public class TesseractDetection
+    {
+        public static string getText(string inputFilePath)
+        {
+            Bitmap image = (Bitmap)System.Drawing.Image.FromFile(inputFilePath);
+            TesseractEngine ocr = new TesseractEngine("tessdata", "eng");
+            BitmapToPixConverter b = new BitmapToPixConverter();
+            Pix p = b.Convert(image);
+            Page page = ocr.Process(image, PageSegMode.Auto);
+            string text = page.GetText();
+            return text;
+        }
+
+        public static string removeWhiteSpaces(string str)
+        {
+            string strReturn = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ' && str[i] != '\t')
+                    strReturn += str[i];
+            }
+            return strReturn;
+        }
+
+        public static string removeNewLineCharacters(string str)
+        {
+            string strReturn = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != '\n')
+                    strReturn += str[i];
+            }
+            return strReturn;
+        }
+
+        public static string getUserIDString(string str)
+        {
+            int i = 0;
+            while (str[i] != '\\')
+            {
+                i++;
+                if (i >= str.Length)
+                    return "";
+            }
+            i++;
+            string strReturn = "";
+            while (i < str.Length && (str[i] == '|' || str[i] == '/' || str[i] == 'l'))
+            {
+                strReturn += str[i];
+                i++;
+            }
+            if (strReturn == "" && i < str.Length)
+                return getUserIDString(str);
+            return strReturn;
+        }
+
+        public static string getImageIDString(string str)
+        {
+            int i = 0;
+            while (str[i] != '#')
+            {
+                i++;
+                if (i >= str.Length)
+                    return "";
+            }
+            i++;
+            string strReturn = "";
+            while (i < str.Length && (str[i] == '|' || str[i] == '/' || str[i] == 'l'))
+            {
+                strReturn += str[i];
+                i++;
+            }
+            if (strReturn == "" && i < str.Length)
+                return getImageIDString(strReturn);
+            return strReturn;
+        }
+    }
+}
