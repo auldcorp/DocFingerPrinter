25b515f5c4ad023b32602c23f45353757b69c73b
darkoscar5 <rubio.27@osu.edu>
2016-10-06 19:48:47 -0400

    Fixed web templating, Routing, and dependancies

---
+++ app/controllers/class_template.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Napkins;
+
+use Symfony\Component\Templating\PhpEngine;
+use Symfony\Component\Templating\TemplateNameParser;
+use Symfony\Component\Templating\Loader\FilesystemLoader;
+
+class WebTemplate extends PhpEngine
+{
+	public $page_title = 'Image Hash';
+	public $page_heading = '';
+	public $page_subheading = '';
+
+	function __construct()
+	{
+		$loader = new FilesystemLoader(__DIR__.'/../templates/%name%');
+		return parent::__construct(new TemplateNameParser(), $loader);
+	}
+
+	function setTitle($title, $overwrite = FALSE)
+	{
+		if($overwrite === FALSE)
+		{
+			$this->page_title = $title . ' | ' . $this->page_title;
+		}
+		else
+		{
+			$this->page_title = $title;
+		}
+		$this->page_heading = $title;
+	}
+	
+	function setTitleHeading($h)
+	{
+		$this->page_heading = $h;
+	}
+	
+	function setTitleSubHeading($h)
+	{
+		$this->page_subheading = $h;
+	}
+	
+	function renderDefault($errors = array())
+	{
+		// Render the default web template
+		$this->addGlobal('page_title', $this->page_title);
+		$this->addGlobal('page_heading', $this->page_heading);
+		$this->addGlobal('page_subheading', $this->page_subheading);
+//		$this->addGlobal('page_content', $page_content);
+		return $this->render('base.php', array('errors' => $errors));
+
+	}
+	
+
+}
---
+++ app/templates/welcome.php
@@ -0,0 +1,20 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Welcome</title>
+	</head>
+	<body>
+		<div id="main" class="container-fluid">
+			<div id="content" class="clear row col-xs-offset-2">
+				<div class="col-md-9">
+				<h2>Welcome</h2>
+			</div>
+		<div class="col-md-6">
+		<h2>About Image Hash</h2>
+		</div>
+
+		</div>
+
+	</body>
+</html>
