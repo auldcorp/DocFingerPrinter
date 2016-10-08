14d8bb671a07a21aa86e6c20226a84adefef1263
Daniel Gratz <gratz.dhg@gmail.com>
2016-10-08 16:01:52 -0400

    added upload controller

--- app/app/controllers/UploadController.php
+++ app/app/controllers/UploadController.php
@@ -1,3 +1,19 @@
 <?php
+namespace Napkins;
 
+require_once(__DIR__ . '/class_template.php');
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+Class UploadController {
+	public function uploadAction(Request $request, Application $app) {
+		return Response('YOLOOOOOOOOOO');
+	}
+	public function uploadView(Request $request, Application $app)
+	private function upload(Application $app) {
+
+	}
+}
 ?>
--- app/index.php
+++ app/index.php
@@ -2,6 +2,10 @@
 date_default_timezone_set('America/New_York');
 require_once __DIR__.'/vendor/autoload.php';
 
+define('DEBUG', TRUE);
+ini_set('display_errors', 'On');
+ini_set('html_errors', 'On');
+ini_set('log_errors', 'On');
 
 $app = new Silex\Application();
 
@@ -19,9 +23,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path' => __DIR__.'/app/templates',
 ));
 
-$app->get('/pleasework', function() {
-        return 'yolo! napkins!';
-});
+$app->get('/pleasework','Napkins\\UploadController::uploadAction');
 
 $app->get('/', function() use($app) {
 	return $app['twig']->render('base.html', array(
