c064ed9d072764287971b8b896fd867edec41102
darkoscar5 <rubio.27@osu.edu>
2016-10-08 21:32:23 -0400

    A lot of email stuff, index.php fixes

--- app/controllers/NotificationController.php
+++ app/controllers/NotificationController.php
@@ -1,3 +1,24 @@
 <?php
 
-?>
+namespace Napkins;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Napkins\WebTemplate;
+
+class NotificationController
+{
+	public function email(Application $app)
+	{
+		$message = \Swift_Message::newInstance()
+			->setSubject('Image Hash: Potential Image Match')
+			->setFrom(array('noreply@auldcorporation.com'))
+			->setTo(array('dejesus.21@osu.edu'))
+			->setBody('Potential Image Match found at arealwebsite.com fo Image:1');
+		$app['mailer']->send($message);
+
+		return new Response('Email has ben sent!', 201);
+	}
+}
--- app/index.php
+++ app/index.php
@@ -2,6 +2,8 @@
 date_default_timezone_set('America/New_York');
 require_once __DIR__.'/vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 
 $app['debug'] = TRUE;
@@ -20,18 +22,28 @@ else
 	ini_set('og_errors', 'On');
 }
 
+$app['swiftmailer.options'] = array(
+	'host' => 'localhost',
+	'port' => '25',
+	'username' => 'username',
+	'password' => 'password',
+	'encryption' => null,
+	'auth_mode' => null
+);
+
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
-        'dbname' => 'nakpins',
-	'user' => 'nakpin_holder',
+        'dbname' => 'napkins',
+	'user' => 'napkin_holder',
 	'password' => 'e7bQ47yk3nebtlAqVEp7C8I',
-	'host' => 'localhost',
+	'host' => '127.0.0.1',
 	'driver' => 'pdo_mysql',
     ),
 ));
 
+$app->register(new Silex\Provider\SwiftmailerServiceProvider());
 
-$app->register(new Silex\Provider\SessionServiceProvider);
+$app->register(new Silex\Provider\SessionServiceProvider());
 
 $app->get('/pleasework', function() {
         return 'yolo! napkins!';
@@ -40,6 +52,10 @@ $app->get('/pleasework', function() {
 $app->get('/', 'Napkins\\IndexController::defaultView');
 
 $app->get('/login', 'Napkins\\LoginController::defaultAction')->value('action', 'login_view');
+
+$app->get('/email', 'Napkins\\NotificationController::email');
+
+$app->post('/login', 'Napkins\\LoginController::defaultAction')->value('action', 'login');
 
 $app->before( function ($request) {
 	$request->getSession()->start();
