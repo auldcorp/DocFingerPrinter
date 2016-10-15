560febb2535673a975005e6783eb255b0e6ef02d
darkoscar5 <rubio.27@osu.edu>
2016-10-15 17:43:39 -0400

    NotificationConroller now uses session email

--- app/controllers/NotificationController.php
+++ app/controllers/NotificationController.php
@@ -12,10 +12,12 @@ class NotificationController
 {
 	public function email(Application $app)
 	{
+		$to = 'dejesus.21@osu.edu';
+		if(!null == $email = $app['session']->get('email')) $to = $email;
 		$message = \Swift_Message::newInstance()
 			->setSubject('Image Hash: Potential Image Match')
 			->setFrom(array('noreply@auldcorporation.com'))
-			->setTo(array('dejesus.21@osu.edu'))
+			->setTo(array($to))
 			->setBody('Potential Image Match found at arealwebsite.com fo Image:1');
 		$app['mailer']->send($message);
 
