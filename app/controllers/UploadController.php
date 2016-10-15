88d9ab7b03efb894f1ba43868752ff467a880a0b
darkoscar5 <rubio.27@osu.edu>
2016-10-15 18:06:11 -0400

    Sample guideline for inserting images into the db

--- app/controllers/UploadController.php
+++ app/controllers/UploadController.php
@@ -6,6 +6,8 @@ require_once(__DIR__ . '/class_template.php');
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+//use Jessegers\ImageHash\Implementations\DifferenceHash;
+//use Jessegers\ImageHash\ImageHash;
 
 Class UploadController {
 	public function uploadAction(Request $request, Application $app) {
@@ -31,6 +33,31 @@ Class UploadController {
 			}
 		}
 		$return = ['succeeded'=>$succeeded,'failed'=>$failed];
+
+		/*$implementaion = new DifferenceHash;
+		$hasher = new ImageHash($implementaion);
+
+		$sql = 'INSERT INTO images (hash, email, image, date) VALUES ( :hash, :email, :image, :date)';
+		foreach($succeeded as $row)
+		{
+			$stmt = $app['db']->prepare($sql);
+			$hash = $hasher->hash('path/to/image.jpg');
+			$stmt->bindValue('hash', $hash);	
+			$stmt->bindValue('email', $email);
+			$stmt->bindValue('image', 'path/to/image.jpg');
+			$stmt->bindValue('date', date(); //See php date
+
+			if($stmt->execute())
+			{
+				//Success
+			}
+			else
+			{
+				//Failure
+			}
+		}
+		*/
+
 		$page_content = $templating->render('upload_image_success.php',$return);
 		$templating->setTitle('Upload Image');
 		$templating->addGlobal('page_content', $page_content);
