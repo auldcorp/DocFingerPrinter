0dbd875cbbfe5eb088158758bcbef79ae31ba71b
Daniel Gratz <gratz.dhg@gmail.com>
2016-11-05 20:55:00 -0400

    initial work on fingerprint list

--- app/templates/image_list.php
+++ app/templates/image_list.php
@@ -8,6 +8,19 @@
 	</head>
 	<body>
 		<div id="main" class="container-fluid">
+<div class="dropdown">
+  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+    Dropdown
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
 			<div id="content" class="clear col-xs-offset-2">
 						<h2>Uploaded Images</h2>
 						<form method="post" action="processImages">
@@ -47,7 +60,7 @@ if(isset($images)&&!empty($images)) {
 	echo '</div>';
 }
 ?>
-						<button class="btn btn-lg btn-primary col-md-5" type="submit">Process Images</button>
+						<button class="btn btn-lg btn-primary col-md-3" type="submit">Process Images</button>
 					</form>
 			</div>
 		</div>
