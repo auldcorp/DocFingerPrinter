917ccbc1ecc4f5e6aba3110a94547ea0ef490128
Daniel Gratz <gratz.dhg@gmail.com>
2016-11-11 16:29:58 -0500

    added fingerprinter view

--- app/controllers/FingerprintController.php
+++ app/controllers/FingerprintController.php
@@ -1,3 +1,50 @@
 <?php
+namespace Napkins;
 
-?>
+require_once(__DIR__ . "/class_template.php");
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Jenssegers\ImageHash\ImageHash;
+
+Class FingerprintController {
+private $errors = [];
+
+public function fingerprintView(Request $request, Application $app) {
+	if(null == $email = $app["session"]->get("email"))
+	{
+		$app["session"]->set("dir", "fingerprints");
+		return $app->redirect("login");
+	}
+	$fingerprintDir = $app["imageDirBase"].$email."/fingerprints";
+
+	$sql = "SELECT * FROM fingerprints WHERE email = :email";
+	$templating = new WebTemplate();
+
+	$userImages = $app["db"]->fetchAll($sql, Array("email" => $email));
+	for($i = 0; $i < count($userImages); ++$i) {
+		//THIS NEEDS CHANGED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		$imageFile = $imageDir."/".$userImages[$i]["name"].".".$userImages[$i]["extension"];
+		$userImages[$i]["imageFile"] = base64_encode(file_get_contents($imageFile));
+		if($userImages[$i]["imageFile"] == False) {
+			array_push($this->errors, "Finger print file ".$userImages[$i]["orig_name"]." was not found");
+		}
+	}
+	//	var_dump($userImages);
+	$return = ["images"=>$userImages];
+	$page_content = $templating->render("fingerprint_list.php", $return);
+
+	$templating->setTitle("Upload Image");
+	$templating->addGlobal("page_content", $page_content);
+	$templating->addGlobal("login", TRUE);
+//	$retErr = [];
+//	if(isSet($this->errors)) {
+//		$retErr = $this->errors;
+//		$this->errors = [];
+//	}
+	return $templating->renderDefault($this->errors);
+
+
+}
+}
--- app/templates/fingerprint_list.php
+++ app/templates/fingerprint_list.php
@@ -1,28 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Uploaded Image</title>
+	<title>Fingerprints</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	</head>
 	<body>
 		<div id="main" class="container-fluid">
-<div class="dropdown">
-  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-    Dropdown
-    <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-    <li><a href="#">Action</a></li>
-    <li><a href="#">Another action</a></li>
-    <li><a href="#">Something else here</a></li>
-    <li role="separator" class="divider"></li>
-    <li><a href="#">Separated link</a></li>
-  </ul>
-</div>
 			<div id="content" class="clear col-xs-offset-2">
-						<h2>Uploaded Images</h2>
+						<h2>Fingerprints</h2>
 						<form method="post" action="processImages">
 <?php
 if(isset($images)&&!empty($images)) {
@@ -41,27 +28,11 @@ if(isset($images)&&!empty($images)) {
 		echo "<div class='col-md-1'>"."<label><input type='checkbox' value=delete id='".$image["hash"]."' name= '".$image["hash"]."'> Delete</label>"."</div>";
 		echo '</div>';
 		echo '</div>';
-		echo '<div id="found'.$image["hash"].'" class="nav-collapse collapse">';
-		if(count($image["found"])==0) {
-			echo '<div class="panel-footer">No matches found</div>';
-		} else {
-			echo '<div class="panel-footer">'.count($image["found"])." match found".'</div>';
-		}
-		echo '<div class="panel-body">';
-		foreach($image["found"] as $found) {
-			echo '<div class="row">';
-			echo '<div class="col-md-2">'.$found["date"].'</div>';
-			echo '<div class="col-md-8">'.$found["address"]."</div>";
-			echo '<div class="col-md-8">'.$found["grade"]."</div>";
-			echo '</div>';
-		}
-		echo '</div>';
-		echo '</div>';
 	}
 	echo '</div>';
 }
 ?>
-						<button class="btn btn-lg btn-primary col-md-3" type="submit">Process Images</button>
+						<button class="btn btn-lg btn-primary col-md-3" type="submit">Process Fingerprints</button>
 					</form>
 			</div>
 		</div>
